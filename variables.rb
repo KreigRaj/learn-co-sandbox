3a62dbed02462d3fda3fdd3d57a92e0add85bb48
Kreig Rajaram <krajaramdc@gmail.com>
2018-10-02 16:01:39 +0000

    automatically backed up by learn

---
+++ variables.rb
@@ -0,0 +1,4 @@
+def greet_three_people(name_one, name_two, name_three)
+  puts "Hi everyone! Hi "+ name_one + ", hi " + name_two + ", hi " + name_three
+end 
+greet_three_people("Kreig", "Rachel", "Lady")
